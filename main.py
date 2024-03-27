60d4c89202850ff7035ccb7e8b53ee3e300dd367
Haveadream1 <260602david@gmail.com>
2024-03-27 15:02:20 +0100

    Change ext to py

---
+++ main.py
@@ -0,0 +1,13 @@
+# CLI game
+# Game with 5 tries
+# Goal's to discover the number
+    #   random number
+    #   range > set up difficulty
+
+# After each try the computer give a statement
+    #   Win > we found the number
+    #   Bigger > our num is lower
+    #   Lower > our num is bigger
+    #   Loose > we pass the 5th try
+
+# The number we give can be repeated
